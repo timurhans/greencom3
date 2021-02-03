8f86512a97b385af3b3ea57d2336f7f5269c4980
timurhans <65465543+timurhans@users.noreply.github.com>
2021-02-02 21:56:08 -0300

    Update admin.py

--- params/admin.py
+++ params/admin.py
@@ -2,17 +2,11 @@ from django.contrib import admin
 
 # Register your models here.
 
-<<<<<<< HEAD
-from .models import (ColecaoB2b,ColecaoErp,Banner)
-=======
+
 from .models import (ColecaoB2b,ColecaoErp,Banner,Periodo)
->>>>>>> 8a45137b9376bd9d457abf976e6a9af0f9dc5c97
 
 
 admin.site.register(ColecaoB2b)
 admin.site.register(ColecaoErp)
 admin.site.register(Banner)
-<<<<<<< HEAD
-=======
 admin.site.register(Periodo)
->>>>>>> 8a45137b9376bd9d457abf976e6a9af0f9dc5c97
